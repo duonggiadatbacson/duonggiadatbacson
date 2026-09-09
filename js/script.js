f4b07170752b603574330643ef907ef4bff0885f
quannguyen85 <quannguyensky3105@gmail.com>
2026-09-09 08:52:35 +0700

    giao dien web

---
+++ js/script.js
@@ -0,0 +1,10 @@
+﻿// ==========================================================
+// DUONG GIA DAT HOMESTAY
+// MAIN JAVASCRIPT
+// ==========================================================
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    console.log("Dương Gia Đạt Homestay website loaded.");
+
+});
